49fd88397588e6a154d4349d48b1e92ad59562e7
Malihashahi <maliha.shahi.03@gmail.com>
2025-07-19 21:26:47 +0430

    modfiy some code

--- stand_land/settings.py
+++ stand_land/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #my app
+    #my application
 
     'home_app.apps.HomeAppConfig',
     'account.apps.AccountConfig'
