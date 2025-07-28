ed8bdf49ee53ae7c2b7959fd11f68374452f30a8
Malihashahi <maliha.shahi.03@gmail.com>
2025-07-28 21:45:45 +0430

    add authentication on users permissions

--- account/views.py
+++ account/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth import aauthenticate , login
 # Create your views here.
 def login(request):
-  return render(request , 'account/login.html' ,{})
+    if request.method == "POST":
+       username =  request.POST.get('username')
+       password = request.POST.get('password')
+
+
+    return render(request , 'account/login.html' ,{})
