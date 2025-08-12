20ed9198bc8718b40e25941735cfe15dda67df30
Malihashahi <maliha.shahi.03@gmail.com>
2025-08-12 21:44:08 +0430

    create register form

--- account/views.py
+++ account/views.py
@@ -19,6 +19,11 @@ def user_login(request):
     return render(request , 'account/login.html' ,{})
 
 
+def user_register(request):
+    return render(request , "account/register.html" , {})
+
+
+
 
 def user_logout(request):
     logout(request)
