5a62162a7cfc94499bde428ec31c6e6b2b00ef0c
Malihashahi <maliha.shahi.03@gmail.com>
2025-08-24 11:11:13 +0430

    complete the moduls

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,10 @@ class Article(models.Model):
     image = models.ImageField(upload_to="image/article")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now= True)
+
+
+
+
+
+def __str__(self):
+     return  f"{self.title } - {self.body}"
