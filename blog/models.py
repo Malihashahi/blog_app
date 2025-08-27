8e285e578a0a6ca6a7e447b17604e03493c09ec5
Malihashahi <maliha.shahi.03@gmail.com>
2025-08-27 22:56:10 +0430

    add the author filed for models

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
 class Article(models.Model):
+    author = models.ForeignKey(User ,on_delete=models.CASCADE, )
     title = models.CharField(max_length=30)
     body = models.CharField(max_length=608)
     image = models.ImageField(upload_to="image/article")
