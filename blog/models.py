040c1df64e5af143df64b66484b080b0aacba16b
Malihashahi <maliha.shahi.03@gmail.com>
2025-09-01 10:06:44 +0430

    add some parameter for models options

--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 
 class Article(models.Model):
     author = models.ForeignKey(User ,on_delete=models.SET_NULL ,null=True , blank=True)
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=30 , help_text="Enter A volid title" , unique=True , db_column="mytitle")
     body = models.CharField(max_length=608)
     image = models.ImageField(upload_to="image/article")
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True , editable=False)
     updated = models.DateTimeField(auto_now= True)
 
 
