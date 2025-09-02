b7ed35f55d9c74c219cde9bd487396e1c045c034
Malihashahi <maliha.shahi.03@gmail.com>
2025-09-02 22:03:05 +0430

    add timezone for pub_date filed

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
 
 class Article(models.Model):
     author = models.ForeignKey(User ,on_delete=models.SET_NULL ,null=True , blank=True)
-    title = models.CharField(max_length=30 , help_text="Enter A volid title" , unique=True , db_column="mytitle")
+    title = models.CharField(max_length=30 , help_text="Enter A volid title" , unique=True , db_column="mytitle" , unique_for_date='pub_date')
     body = models.CharField(max_length=608)
     image = models.ImageField(upload_to="image/article")
     created = models.DateTimeField(auto_now_add=True , editable=False)
     updated = models.DateTimeField(auto_now= True)
-
+    pub_date = models.DateField(default=timezone.now())
 
 
 
