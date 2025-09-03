8ef787e4fd92ad7f715d443845ce0a17cfa076fb
Malihashahi <maliha.shahi.03@gmail.com>
2025-09-03 21:46:57 +0430

    modfiy some code

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Article(models.Model):
     author = models.ForeignKey(User ,on_delete=models.SET_NULL ,null=True , blank=True)
-    title = models.CharField(max_length=30 , help_text="Enter A volid title" , unique=True , db_column="mytitle" , unique_for_date='pub_date')
+    title = models.CharField(max_length=30 , db_column="mytitle" , unique_for_date='pub_date')
     body = models.CharField(max_length=608)
     image = models.ImageField(upload_to="image/article")
     created = models.DateTimeField(auto_now_add=True , editable=False)
