7dc34455857c29fe3966fe90bed8875ef3973994
Malihashahi <maliha.shahi.03@gmail.com>
2025-09-04 22:08:15 +0430

    add category models and apply many to many relastionships

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your models here.
+class Category(models.Model):
+     title = models.CharField(max_length=100)
+     created = models.DateTimeField(auto_now_add=True)
+
+
+def __str__(self):
+     return self.title
+
 
 
 class Article(models.Model):
     author = models.ForeignKey(User ,on_delete=models.SET_NULL ,null=True , blank=True)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=30 , db_column="mytitle" , unique_for_date='pub_date')
     body = models.CharField(max_length=608)
     image = models.ImageField(upload_to="image/article")
