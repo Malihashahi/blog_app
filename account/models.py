a7b3caf2b00165d5c4b64c564c4656d48f0df09e
Malihashahi <maliha.shahi.03@gmail.com>
2025-09-09 22:27:53 +0430

    work one to one relationships

--- account/models.py
+++ account/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class User(models.Model):
    fullname = models.CharField(max_length=44)
+
+
+
+class Profile(models.Model):
+   uset = models.OneToOneField()
