245a941faeae1d791f79753a935f8cfbec0b1dc7
Malihashahi <maliha.shahi.03@gmail.com>
2025-09-20 22:23:39 +0430

    work with bonary filed

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,7 @@ class Article(models.Model):
     created = models.DateTimeField(auto_now_add=True , editable=False)
     updated = models.DateTimeField(auto_now= True)
     pub_date = models.DateField(default=timezone.now())
+    myfile = models.BinaryField(null=True)
 
 
 
--- home_app/views.py
+++ home_app/views.py
@@ -3,4 +3,7 @@ from blog.models import Article
 # Create your views here.
 def home(request):
     articles = Article.objects.all()
+    article = Article.objects.get(id = 16)
+    article.myfile = bytes('hello me ' ,'utf-8')
+    article.save()
     return render(request ,"home_app/index.html", {'articles': articles} )
