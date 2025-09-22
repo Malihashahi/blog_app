c22769aa18a5423b69b3fcec62a5c8a62aed1eaf
Malihashahi <maliha.shahi.03@gmail.com>
2025-09-22 22:40:05 +0330

    modfiy some code

--- home_app/views.py
+++ home_app/views.py
@@ -4,6 +4,6 @@ from blog.models import Article
 def home(request):
     articles = Article.objects.all()
     article = Article.objects.get(id = 16)
-    article.myfile = bytes('hello me ' ,'utf-8')
+    article.myfile = bytes('hello  ' ,'utf-8')
     article.save()
     return render(request ,"home_app/index.html", {'articles': articles} )
