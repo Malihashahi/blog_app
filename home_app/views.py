b382dc2e7a864efc70428b3fcb1a0393a49dd00d
Malihashahi <maliha.shahi.03@gmail.com>
2025-09-25 22:35:17 +0330

    use of filter

--- home_app/views.py
+++ home_app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from blog.models import Article
+from blog.models import Article , New
 # Create your views here.
 def home(request):
-    articles = Article.objects.all()
-    article = Article.objects.get(id = 16)
-    article.myfile = bytes('hello  ' ,'utf-8')
-    article.save()
+    articles = Article.objects.filter(status =True)
+   
     return render(request ,"home_app/index.html", {'articles': articles} )
