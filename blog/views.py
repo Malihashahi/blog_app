dcade960c5daa68b481081399b013f6bd4f8e31d
Malihashahi <maliha.shahi.03@gmail.com>
2025-10-02 21:25:05 +0330

    create view for article detail

--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render
+from blog.models import Article
 
+
+
+
+
+def post_detail(request , pk):
+   article = Article.objects.get(id = pk)
+   return render(request , "blog/article_detail.html" , {'article' : article})
