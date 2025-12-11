123d775ff55b5f04a048acdbb63b34c7a7bdef0c
Malihashahi <maliha.shahi.03@gmail.com>
2025-12-11 20:55:50 +0330

    complate the  commet moduls

--- blog/models.py
+++ blog/models.py
@@ -74,4 +74,13 @@ class New(models.Model):
 
 
 class Comment(models.Model):
-     article = models.ForeignKey()
+     article = models.ForeignKey(Article , on_delete=models.CASCADE , related_name='comments')
+     User = models.ForeignKey(User ,on_delete=models.CASCADE , related_name='comments')
+     body = models.TextField()
+     created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
+     def __str__(self):
+          return self.body[:50]
