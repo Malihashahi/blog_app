7c3f0023d877003e96666e9a403d8132da8d480e
Malihashahi <maliha.shahi.03@gmail.com>
2025-12-27 21:44:03 +0330

    add the url on action  form

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
   path('detail/<int:pk>' , views.post_detail , name ="article_detail"),
   path('list' , views.article_list , name ="article_list"),
-  path('category/<int:pk>' , name="category_detail")
+  path('category/<int:pk>' , name="category_detail"),
+  path('search/', views.search , name "search_articles"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -29,4 +29,8 @@ def category_deatial(request , pk=None):
 
 def search(request):
    q = request.GET.get('q')
-   articles = Article.objects.filter(title__contains = q)
+   articles = Article.objects.filter(title__icontains = q)
+   page_number = request.GET.get('page')
+   paginator = Paginator(articles , 1)
+   objects_list = paginator.get_page(page_number)
+   return render(request, 'blog/articles_list.html' ,{'articles' : articles})
