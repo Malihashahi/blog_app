b0d8607e7bb0b575be89e5f59159cbec77f18630
Malihashahi <maliha.shahi.03@gmail.com>
2026-01-07 22:18:29 +0330

    modify some code

--- blog/views.py
+++ blog/views.py
@@ -39,8 +39,8 @@ def search(request):
 
 
 def contactus(request):
-   if request.methond == 'POST':
-      form = ContactUs(request.POST)
+   if request.methond == 'GET':
+      form = ContactUs(request.GET)
       if form.is_valid():
          print(form.cleaned_data['text'])
          massage = Massage.object()
