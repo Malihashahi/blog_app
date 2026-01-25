178e169115c6c2573782c4ecf3922279f8a9ce17
Malihashahi <maliha.shahi.03@gmail.com>
2026-01-25 22:28:05 +0330

    add python form file

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.core.validators import ValidationError
+
+
+
+class ContactUsForm(forms.Form):
+  name = forms.CharField(max_length=10, label='your name')
+  text = forms.CharField(max_length=10 , label='your massage')
+
+
+  def clean(self):
+    name = self.cleaned_data.get('name')
+    text = self.cleaned_data.get('text')
+    if name == text :
+        raise ValidationError('')
