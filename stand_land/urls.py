466297c93cd608d7424e335e31784e662b27b416
Malihashahi <maliha.shahi.03@gmail.com>
2026-02-15 22:02:39 +0330

    change the url

--- stand_land/urls.py
+++ stand_land/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home_app.urls')),
     path('', include('account.urls')),
-    path('articles/', include('blog.urls'))
+    path('article/', include('blog.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT )
